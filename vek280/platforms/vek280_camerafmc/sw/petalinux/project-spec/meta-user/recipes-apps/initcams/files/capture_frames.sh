db6db8c16c523d1677c2023458049ee27b318451
Mario Bergeron <grouby177@gmail.com>
2025-04-11 09:53:26 -0400

    vek280: platforms: vek280_camerafmc: replace capture_frames.sh with capture_frames_libcamera_bgr.sh|capture_frames_yavta_bgr.sh|capture_frames_yavta_yuyv.sh.

--- vek280/platforms/vek280_camerafmc/sw/petalinux/project-spec/meta-user/recipes-apps/initcams/files/capture_frames.sh
+++
@@ -1,9 +0,0 @@
-cam --list
-cam -c1 -C1 --stream width=1920,height=1080,pixelformat=BGR888  -Fcam0.bin
-ffmpeg -f rawvideo -s 1920x1080 -pix_fmt rgb24 -i cam0.bin cam0.png
-cam -c2 -C1 --stream width=1920,height=1080,pixelformat=BGR888  -Fcam1.bin
-ffmpeg -f rawvideo -s 1920x1080 -pix_fmt rgb24 -i cam1.bin cam1.png
-cam -c3 -C1 --stream width=1920,height=1080,pixelformat=BGR888  -Fcam2.bin
-ffmpeg -f rawvideo -s 1920x1080 -pix_fmt rgb24 -i cam2.bin cam2.png
-cam -c4 -C1 --stream width=1920,height=1080,pixelformat=BGR888  -Fcam3.bin
-ffmpeg -f rawvideo -s 1920x1080 -pix_fmt rgb24 -i cam3.bin cam3.png
